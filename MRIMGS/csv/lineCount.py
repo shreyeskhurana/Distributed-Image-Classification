e49cd4afbb99f4b319fea11a86ab6b86c7ad2562
Sandarsh Srivastav <sandarsh@Sandarshs-MacBook-Pro.local>
2017-12-10 12:30:06 -0500

    Python Script

---
+++ MRIMGS/csv/lineCount.py
@@ -0,0 +1,14 @@
+file = open("L6_1_965381.csv", "r")
+w_file = open("small_50m.csv", "w")
+print "Entering for loop"
+count = 0
+end = 8000
+for line in file.readlines():
+    count +=1
+    w_file.write(line)
+    if count >= end:
+        break
+print count / (6 * 1024)
+print "Closing File"
+file.close()
+w_file.close()
